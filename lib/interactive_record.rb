912e0789dabfbca92c923ae7a3cb0dcd7cc5d91d
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-03 20:16:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -16,9 +16,7 @@ class InteractiveRecord
   end 
   column_names.compact
 end 
-self.column_names.each do |col_names|
-  attr_accessor col_names.to_sym
-end 
+
 
 def initialize(options = {})
 options.each do |property,value|
