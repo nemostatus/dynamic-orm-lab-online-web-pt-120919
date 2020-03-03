4309f09de2771d6c987ae377fbe0c8ff7a2fc6f6
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-03 20:05:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,6 +43,6 @@ def save
   sql = "INSERT INTO #{table_name_for_insert}
   (#{col_names_for_insert}) VALUES #{values_for_insert})"
   DB[:conn].execute(sql)
-  @id =DB[:conn]
+  @id =DB[:conn].execute()
 end 
 end 
