7797689db22a85ea21a28f9c01ced9c7c89e900a
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-03 19:56:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ def table_name_for_insert
   self.class.table_name
 end 
 def col_names_for_insert
-  self.class.column_names.delete_if {|col| col == "id"}.join(" , ")
+  self.class.column_names.delete_if {|col| col == "id"}.join(", ")
 end 
 
 end 
