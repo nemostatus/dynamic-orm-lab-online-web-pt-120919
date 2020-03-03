3fbd971e6e05077389b4385ef8640a9d15b225be
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-03 20:17:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,12 +22,15 @@ options.each do |property,value|
   self.send("#{property}=",value)
 end
 end 
+
 def table_name_for_insert
   self.class.table_name
 end 
+
 def col_names_for_insert
   self.class.column_names.delete_if {|col| col == "id"}.join(", ")
 end 
+
 def values_for_insert 
   values =[]
   self.class.column_names.each do |col_name|
